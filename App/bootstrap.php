bf77e816609a33dbe997c5d19a0e7c8e16ba0772
Scott <scottfusilier@gmail.com>
2016-04-23 21:24:50 -0400

    move application execution to index.php, this will allow bootstrap.php to be easily included for scripts

--- App/bootstrap.php
+++ App/bootstrap.php
@@ -1,14 +1,8 @@
 <?php
-/* */
+/* autoloading and configuration */
 
 $loader = include dirname(__DIR__).'/vendor/autoload.php';
 $loader->add('App\\', dirname(__DIR__));
 
 include __DIR__.'/Config/config.php';
 include __DIR__.'/Config/routes.php';
-
-session_start();
-
-Lib\Container\AppContainer::register(new App\Handler\AppErrorHandler);
-
-Lib\App::getInstance()->run($routes);
