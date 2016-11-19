2164c067a3867034028734c7b5ee34438217de17
Scott <scottfusilier@gmail.com>
2016-11-19 13:03:54 -0500

    drop 'type' on data based routes

--- App/Controller/AppDataController.php
+++ App/Controller/AppDataController.php
@@ -5,14 +5,12 @@ abstract class AppDataController extends AppController
 {
     public function data($args)
     {
-        if (!empty($args['type']) && $args['type'] === __FUNCTION__) {
-            if (!empty($args['action']) && method_exists($this, $args['action'])) {
-                $method = $args['action'];
-                if ($this->isOpenACL($method) || $this->isAuthorized($method)) {
-                    return $this->$method($args);
-                }
-                return $this->unAuthorized();
+        if (!empty($args['action']) && method_exists($this, $args['action'])) {
+            $method = $args['action'];
+            if ($this->isOpenACL($method) || $this->isAuthorized($method)) {
+                return $this->$method($args);
             }
+            return $this->unAuthorized();
         }
         return $this->renderFourOhFour();
     }
