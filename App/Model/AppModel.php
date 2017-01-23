a2eaba44aaebd79d5de7855c4ac886c111c5ba64
Scott <scottfusilier@gmail.com>
2017-01-22 20:47:51 -0500

    update for model interface refactor

--- App/Model/AppModel.php
+++ App/Model/AppModel.php
@@ -1,9 +1,9 @@
 <?php
 namespace App\Model;
 
-use Lib\Model\Model;
+use Lib\Model\SqlModel;
 
-abstract class AppModel extends Model
+abstract class AppModel extends SqlModel
 {
     public function setup()
     {
