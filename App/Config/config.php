6c8dee104d56bfa7f84b9880f143d2c77d5d5313
Scott <scottfusilier@gmail.com>
2017-02-13 21:23:01 -0500

    refactor to use improved connection container, register callables that return connection

--- App/Config/config.php
+++ App/Config/config.php
@@ -1,10 +1,30 @@
 <?php
 /*
- * config.php -- define constants and system params here
+ * config params here
  */
 
-// database configuration namespace
-define('DBCONFIG_NAMESPACE', '\\App\\Config\\DatabaseConfig');
+// Register database configuration callables. Callable should return a new connection object.
+// NOTE: ConnectionContainer will call the Callable on first access and then return the same connection instance on every subsequent access.
+Lib\Container\ConnectionContainer::register('default',
+function () {
+    try {
+        $obj = new \PDO('mysql:host=172.16.1.17;dbname=Example_DB;charset=utf8','ExampleUser','ExamplePass',[]);
+    } catch (\PDOException $e) {
+        die('Connection Error');
+    }
+    return $obj;
+});
+/*
+Lib\Container\ConnectionContainer::register('another-connection',
+function () {
+    try {
+        $obj = new \PDO('mysql:host=localhost;dbname=Example_Database;charset=utf8','ExampleUser','ExamplePass',[]);
+    } catch (\PDOException $e) {
+        die('Connection Error');
+    }
+    return $obj;
+});
+*/
 
 // set default timezone
 date_default_timezone_set('America/New_York');
